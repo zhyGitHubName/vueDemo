a307f45e08bac349ae472bc954e325ec280c1ffc
zhyGitHubName <1718516862@qq.com>
2019-09-08 11:30:15 +0800

    dd

---
+++ src/components/AddPoint.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <p>hello nihao</p>
+        <p>{{ resultStr }}</p>
+        <p @click="backBtnClick">fanhui</p>      
+    </div>
+</template>
+<script>
+export default{
+        components:{
+                
+        },
+        data: () =>({
+            resultStr: '',
+        }),
+        methods:{
+              backBtnClick(){
+                  this.$router.go(-1);
+              }
+        },
+        created(){
+                this.resultStr = this.$route.query.plan;
+               
+        },
+        destroyed(){
+
+        }
+}
+</script>
+<style scoped>
+
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h2>Essential Links</h2> 
+    <button @click="btnclick">点击跳转</button>   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: () =>({
+       resultStr: 'loading...',
+  }),
+  methods:{
+    btnclick(){
+     
+      this.$router&&this.$router.push({
+            path: 'AddPoint', 
+            query: { plan: '123' }
+        })
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
